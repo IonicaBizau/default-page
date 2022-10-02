a50047f1fde3f6811b7bf3b944937114c3e1bbd6
toyin <toyinmuhammed50@gmail.com>
2022-10-03 00:01:14 +0200

    Change folder structure + code splitting

---
+++ react-version/src/components/SubHeading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SubHeading() {
+    return (
+        <p class="lead">Created with React, CSS & &hearts; by <a href="http://ionicabizau.net/">Ionică Bizău</a>.
+        </p>
+    )
+}
+
+export default SubHeading
--- react-version/src/index.js
+++ react-version/src/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Heading from './components/Heading';
+import SubHeading from './components/SubHeading';
 
 // CSS
 import './index.css';
 
 // Jumbotron container
-function Jumbotron(){
+function Jumbotron() {
   return (
     <>
-      <Heading/>
-      <SubHeading/>
+      <Heading />
+      <SubHeading />
     </>
   );
 }
-// Heading component
-function Heading(){
-  return(
-    <h1>Hello World</h1>
-  );
-}
+// // Heading component
+// function Heading(){
+//   return(
+//     <h1>Hello World</h1>
+//   );
+// }
 // Subheading component
-function SubHeading(){
-  return(
-    <p class="lead">Created with React, CSS & Love by <a href="http://ionicabizau.net/">Ionică Bizău</a>.
-    </p>
-  );
-}
+// function SubHeading() {
+//   return (
+//     <p class="lead">Created with React, CSS & Love by <a href="http://ionicabizau.net/">Ionică Bizău</a>.
+//     </p>
+//   );
+// }
 
-ReactDom.render(<Jumbotron/>, document.querySelector('.jumbotron'));
+ReactDom.render(<Jumbotron />, document.querySelector('.jumbotron'));
 
 // We can of coure make it more component based than demonstrated here
 // ~ Darshan K S
